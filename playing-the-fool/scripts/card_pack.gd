dcac25e131c049bdd152f579c16abe029c6a1e04
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-07 15:51:34 +0800

    Improvements to the rotation animation

--- playing-the-fool/scripts/card_pack.gd
+++ playing-the-fool/scripts/card_pack.gd
@@ -1,14 +1,13 @@
 extends AnimatedSprite2D
 var rotate_data: Array = [false, 0]
 
+
 func _process(delta: float) -> void:
-	if rotate_data[0]:
-		rotation_degrees = move_toward(rotation_degrees, rotate_data[1], abs(rad_to_deg(angle_difference(deg_to_rad(rotation_degrees), deg_to_rad(rotate_data[1])))) / 1.2 * delta)
-		if abs(int(rotation_degrees) - rotate_data[1]) < 10: rotate_data[0] = false
+	if rotate_data[0]: rotation = lerp_angle(rotation, deg_to_rad(rotate_data[1]), 1.2 * delta)
 
 # Старт анимации поворота
 func _on_animation_player_animation_started(anim_name: StringName) -> void:
-	if anim_name == "fall": rotate_data = [true, randi_range(-366, 366)]
+	if anim_name == "fall": rotate_data = [true, randi() % 366]
 
 # Сигнал завершения анимации
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
