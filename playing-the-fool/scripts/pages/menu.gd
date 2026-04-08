3b3a590dd18300b49fbed1a7cd0cb9462fcb0a21
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-08 19:06:39 +0800

    exit the application

--- playing-the-fool/scripts/pages/menu.gd
+++ playing-the-fool/scripts/pages/menu.gd
@@ -2,3 +2,6 @@ extends Control
 
 # Открытие окна статистики
 func _on_statistics_button_down() -> void: get_parent().close_window("statistics")
+
+# Выход из приложения
+func _on_exit_button_down() -> void: get_tree().quit()
