b0dba7c8f2c13d9cb9b055adb17086f56f181a27
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-08 20:12:26 +0800

    changing application icons

--- playing-the-fool/scripts/pages/menu.gd
+++ playing-the-fool/scripts/pages/menu.gd
@@ -2,6 +2,3 @@ extends Control
 
 # Открытие нового окна
 func _on_window_button_down(new_window: String) -> void: get_parent().close_window(new_window)
-
-# Выход из приложения
-func _on_exit_button_down() -> void: get_tree().quit()
