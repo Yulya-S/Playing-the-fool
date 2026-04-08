c157b053cb4645f321ddfdcfd1c3f975680f9433
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-08 15:28:57 +0800

    moving the card drop animation to a separate class

--- playing-the-fool/scripts/fragments/card_pack.gd
+++ playing-the-fool/scripts/fragments/card_pack.gd
@@ -1,13 +1,15 @@
 extends AnimatedSprite2D
-var rotate_data: Array = [false, 0]
-
+# Переменная
+var rotate_data: Array = [false, 0] # Данные для поворота карты
 
 func _process(delta: float) -> void:
-	if rotate_data[0]: rotation = lerp_angle(rotation, deg_to_rad(rotate_data[1]), 1.2 * delta)
+	if rotate_data[0]:
+		rotation = lerp_angle(rotation, deg_to_rad(rotate_data[1]), 1.2 * delta)
+		if abs(fposmod(rotation_degrees, 360) - rotate_data[1]) < 1: rotate_data[0] = false
 
 # Старт анимации поворота
 func _on_animation_player_animation_started(anim_name: StringName) -> void:
-	if anim_name == "fall": rotate_data = [true, randi() % 366]
+	if anim_name == "fall": rotate_data = [true, randi() % 360]
 
 # Сигнал завершения анимации
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
