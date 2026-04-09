5860df1f935560c24c8846605d2e61c27064456d
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-09 13:29:16 +0800

    opening of the play area

--- playing-the-fool/scripts/pages/menu.gd
+++ playing-the-fool/scripts/pages/menu.gd
@@ -2,3 +2,8 @@ extends Control
 
 # Открытие нового окна
 func _on_window_button_down(new_window: String) -> void: get_parent().close_window(new_window)
+
+# Старт игры
+func _on_play_button_down() -> void:
+	Global.delete_child(get_parent(), get_parent().get_child(1))
+	_on_window_button_down("game")
