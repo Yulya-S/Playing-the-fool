651962305a084a3f8d08b5458f264088f6c800c5
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-20 14:39:52 +0800

    arrangement of cards in hand

--- playing-the-fool/scripts/fragments/Hand.gd
+++ playing-the-fool/scripts/fragments/Hand.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name Hand
 @export var height: float = 0
-@export var start_pos: float = 180
+@export var start_pos: float = 120
 @export var weight: float = 952
 @export var show_card: bool = true
 
@@ -12,4 +12,8 @@ func add_card() -> void:
 		get_child(-1).show_card()
 		get_child(-1).start_anim("growth")
 	else: get_child(-1).hide_card()
-	for i in range(get_child_count()): get_child(i).new_pos[0] = start_pos + (weight / get_child_count() * i)
+	_map_shift()
+
+func _map_shift() -> void:
+	for i in range(get_child_count()):
+		get_child(i).new_pos[0] = start_pos + (0.0 if get_child_count() <= 1 else (weight / (get_child_count() - 1)) * i)
