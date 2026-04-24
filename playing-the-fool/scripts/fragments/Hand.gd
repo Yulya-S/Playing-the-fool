d54b6d807910b701677bbe5894b487bb52d9e920
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-24 13:22:02 +0800

    Fixing the deck code

--- playing-the-fool/scripts/fragments/Hand.gd
+++ playing-the-fool/scripts/fragments/Hand.gd
@@ -1,17 +1,23 @@
 extends Node
 class_name Hand
-@export var height: float = 0
-@export var start_pos: float = 120
-@export var weight: float = 952
-@export var show_card: bool = true
+# Настройки руки
+@export var height: float = 0 # Расположение руки по высоте
+@export var start_pos: float = 120 # Отступ руки от левого края
+@export var weight: float = 952 # Ширина руки
+@export var show_card: bool = true # Будут ли показываться цена карты
 
+# Проверка что в руке карт больше 6
+func cards_enough() -> bool: return get_child_count() >= 6
+
+# Добавление карты в руку
 func add_card() -> void:
+	Global.deck.Cards.get_child(-1).reparent(self)
 	get_child(-1).transfer(height)
 	get_child(-1).rotation = 0.0
-	if show_card:
+	if not show_card: get_child(-1).hide_card()
+	else:
 		get_child(-1).show_card()
 		get_child(-1).start_anim("growth")
-	else: get_child(-1).hide_card()
 	_map_shift()
 
 func _map_shift() -> void:
--- playing-the-fool/scripts/fragments/deck.gd
+++ playing-the-fool/scripts/fragments/deck.gd
@@ -1,31 +1,41 @@
 extends Node2D
+# Пути к объектам в сцене
+@onready var CardsCount = $CardsCount
+@onready var DeckTimer = $Timer
 @onready var Cards = $Cards
-# Переменная
-var card: Resource = load("res://scenes/fragments/card.tscn")
-var user_idx: bool = true
+# Участники игры
+@onready var Player = $"../Hand"
+@onready var CI = $"../Computer"
+# Переменные
+var card: Resource = load("res://scenes/fragments/card.tscn") # Путь к сцене карты
+var user_idx: bool = true # Порядок передачи карты
 
+# Включение и отключение таймера раздачи карт
+func _process(delta: float) -> void:
+	if Global.game_state == Global.GameStates.DISTRIBUTION:
+		if DeckTimer.paused: DeckTimer.start()
+		elif (Player.cards_enough() and CI.cards_enough()) or Cards.get_child_count() == 0:
+			Global.game_state == Global.GameStates.PLAY
+			DeckTimer.stop()
+
+# Создание сцены
 func _ready() -> void:
 	Global.deck = self
+	# Создание и перемешивание калоды
 	var card_array: Array = Array(range(1, 37))
 	while len(card_array) > 0:
 		Global.add_obj(Cards, card)
 		Cards.get_child(-1).set_value(card_array.pop_at(randi() % len(card_array)))
 		Cards.get_child(-1).position.x += (36 - len(card_array)) * 0.5
 		Cards.get_child(-1).position.y -= (36 - len(card_array)) * 0.5
-	$Label.set_text(str(Cards.get_child_count()))
-	$Card.hide_shadow(Cards.get_child(0).suit)
+	# Обновление графических данных
+	CardsCount.set_text(str(Cards.get_child_count()))
+	$Suit.hide_shadow(Cards.get_child(0).suit)
 
-func dealing_cards(user: Hand) -> void:
-	Cards.get_child(-1).reparent(user)
-	user.add_card()
-
+# Раздача карт
 func _on_timer_timeout() -> void:
-	dealing_cards($"../Hand" if user_idx else $"../Computer")
+	(Player if user_idx else CI).add_card()
 	user_idx = not user_idx
-	if $"../Computer".get_child_count() >= 18: user_idx = true
-	if $"../Hand".get_child_count() >= 18: user_idx = false
-	if ($"../Hand".get_child_count() >= 18 and $"../Computer".get_child_count() >= 18) or \
-		Cards.get_child_count() == 0:
-			$Timer.stop()
-			Global.game_state = Global.GameStates.PLAY
-	$Label.set_text(str(Cards.get_child_count()))
+	if CI.cards_enough(): user_idx = true
+	elif Player.cards_enough(): user_idx = false
+	CardsCount.set_text(str(Cards.get_child_count()))
