9d9ac79fc3cee199c99fc006a8577dc0afb9a697
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-24 11:44:52 +0800

    Update statistics.gd

--- playing-the-fool/scripts/pages/statistics.gd
+++ playing-the-fool/scripts/pages/statistics.gd
@@ -14,10 +14,8 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if len(lines) != 0:
 		Global.add_obj(LinesContainer, file)
-		set_value(lines.pop_front())
-
-# Применение значения для 
-func set_value(value: Dictionary) -> void:
-	for i in LinesContainer.get_child(-1).get_children():
-		if i.name.to_lower() in value.keys(): i.set_text(str(int(value[i.name.to_lower()])))
-	LinesContainer.get_child(-1).get_child(1).set_text("_USER" + LinesContainer.get_child(-1).get_child(1).text)
+		# Применение значения для строки статистики
+		var value: Dictionary = lines.pop_front()
+		for i in LinesContainer.get_child(-1).get_children():
+			if i.name.to_lower() in value.keys(): i.set_text(str(int(value[i.name.to_lower()])))
+		LinesContainer.get_child(-1).get_child(1).set_text("_USER" + LinesContainer.get_child(-1).get_child(1).text)
