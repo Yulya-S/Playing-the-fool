d7de7a4a7ef3a45eeb3c9e995fd56a15de393b03
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-24 11:55:26 +0800

    Update main.gd

--- playing-the-fool/scripts/main.gd
+++ playing-the-fool/scripts/main.gd
@@ -1,27 +1,31 @@
 extends Node2D
+# Путь к объекту в сцене
+@onready var CardAnimation = $Animation
 # Переменная
 var file: Resource = load("res://scenes/fragments/pack.tscn") # Путь к паку с картачными колодами
 
-# Стартовое создание директорий
+# Создание сцены
 func _ready() -> void:
+	# Настройки
 	Global.main = self
+	$Sprite2D.modulate = Color("#"+Global.config.background_color)
+	# Создание файлов и директорий
 	if not DirAccess.dir_exists_absolute(Global.user_path): DirAccess.make_dir_absolute(Global.user_path)
 	if not FileAccess.file_exists(Global.stats_file_path): Global.store_json(Global.stats_file_path, [])
 	Global.create_config()
-	$Sprite2D.modulate = Color("#"+Global.config.background_color)
 
 # Таймер для анимации
 func _on_timer_timeout() -> void:
-	Global.add_obj($Animation, file)
-	$Animation.get_child(-1).position = Vector2(50 + randi() % 1050, 50 + randi() % 550)
-	$Animation.get_child(-1).frame = 4 + randi() % 36
-	$Animation.get_child(-1).modulate.a = 0.
-	$Animation.get_child(-1).get_child(-1).play("fall")
-	if $Animation.get_child_count() > 11: $Animation.get_child(1 + randi() % 5).get_child(-1).play("hide")
+	Global.add_obj(CardAnimation, file)
+	CardAnimation.get_child(-1).position = Vector2(randi_range(50, 1100), randi_range(50, 550))
+	CardAnimation.get_child(-1).frame = 4 + randi() % 36
+	CardAnimation.get_child(-1).modulate.a = 0.
+	CardAnimation.get_child(-1).start_anim("fall")
+	if CardAnimation.get_child_count() > 11: CardAnimation.get_child(1).start_anim("hide")
 
 # Открытие нового окна
 func close_window(new_window: String) -> void:
 	Global.delete_child(self, get_child(-1))
 	Global.add_obj(self, load("res://scenes/pages/"+new_window+".tscn"))
-	get_child(1).visible = not (new_window == "game")
-	get_child(1).get_child(0).set_paused(new_window == "game")
+	CardAnimation.visible = not (new_window == "game")
+	$Animation/Timer.set_paused(new_window == "game")
