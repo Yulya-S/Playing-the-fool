411343bea1122d643b14e6bd36191312a1ba7348
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-04-24 16:28:27 +0800

    Centralization of cards in hand

--- playing-the-fool/scripts/fragments/Hand.gd
+++ playing-the-fool/scripts/fragments/Hand.gd
@@ -3,7 +3,7 @@ class_name Hand
 # Настройки руки
 @export var height: float = 0 # Расположение руки по высоте
 @export var start_pos: float = 120 # Отступ руки от левого края
-@export var weight: float = 952 # Ширина руки
+@export var max_weight: float = 952 # Ширина руки
 @export var show_card: bool = true # Будут ли показываться цена карты
 
 # Проверка что в руке карт больше 6
@@ -20,5 +20,12 @@ func add_card() -> void:
 
 # Изменение позиции карты
 func _map_shift() -> void:
+	# Получение значения шага между картами
+	var step: float = 0.0
+	if get_child_count() > 0:
+		var weight: float = 100 * (get_child_count() - 1)
+		if weight > max_weight: weight = max_weight
+		step = weight / (get_child_count() - 1)
+	# Применение новой позиции
 	for i in range(get_child_count()):
-		get_child(i).new_pos[0] = start_pos + (0.0 if get_child_count() <= 1 else (weight / (get_child_count() - 1)) * i)
+		get_child(i).new_pos[0] = start_pos + (max_weight - step * get_child_count()) / 2 + step * i
