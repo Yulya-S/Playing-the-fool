1d79b8eb67a1d48f86832f03632fa8c315df3bd6
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-05-04 12:30:51 +0800

    checking the possibility of placing a card on the table

--- playing-the-fool/scripts/Hands/Player.gd
+++ playing-the-fool/scripts/Hands/Player.gd
@@ -13,7 +13,6 @@ func _process(_delta: float) -> void:
 		if len(hovered_cards) == 0 or get_child(hovered_cards.max()) == null: return
 		get_child(hovered_cards.max()).position.y = height - 60
 		# Сброс наведения курсора на карту
-		if hovered_cards.max() in unhovered_cards: get_child(hovered_cards.max()).position.y = height
 		hovered_cards = hovered_cards.filter(func(item): return item not in unhovered_cards)
 		unhovered_cards = []
 
@@ -22,10 +21,9 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click") and len(hovered_cards) > 0: clicked = hovered_cards.max()
 	elif event.is_action_released("click") and clicked != -1:
 		if get_child(clicked) == null: return
-		if not Table.mouse_hover(): get_child(clicked).position = get_child(clicked).new_pos
+		if not Table.mouse_hover() or not Global.table.add_card(get_child(clicked)):
+			get_child(clicked).position = get_child(clicked).new_pos
 		else:
-			get_child(clicked).reparent(Table)
-			Table.get_child(-1).rotate_data = [true, randf_range(-0.2, 0.2)]
 			Table.get_child(-1).start_anim("growth", true)
 			_map_shift()
 		hovered_cards = hovered_cards.filter(_clear_cards_array)
--- playing-the-fool/scripts/fragments/table.gd
+++ playing-the-fool/scripts/fragments/table.gd
@@ -1,4 +1,5 @@
 extends Control
+var card_prices: Array = []
 
 # Создание сцены
 func _ready() -> void: Global.table = self
@@ -11,3 +12,11 @@ func mouse_hover() -> bool:
 # Общая часть проверки позиции курсора мыши
 func _check_pos(mouse_pos: Vector2, idx: int) -> bool:
 	return mouse_pos[idx] > position[idx] and mouse_pos[idx] < position[idx] + size[idx]
+
+func add_card(card: Node) -> bool:
+	if len(card_prices) == 0 or card.price in card_prices:
+		if len(card_prices) == 0: card_prices.append(card.price)
+		card.reparent(self)
+		get_child(-1).rotate_data = [true, randf_range(-0.2, 0.2)]
+		return true
+	return false
