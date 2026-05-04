5cd027a421e20b7b166554dc64792bbc1f5dbe2c
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-05-04 14:34:03 +0800

    Update settings.gd

--- playing-the-fool/scripts/pages/settings.gd
+++ playing-the-fool/scripts/pages/settings.gd
@@ -3,28 +3,30 @@ extends GameWindow
 
 # Применение цвета
 func _ready() -> void:
+	$ColorPicker.color = Color("#"+Global.config.background_color)
+	$DeckType.selected = Global.config.card_pack
+	# Заполнение списка языков для выбора
 	for i in DirAccess.get_files_at("res://data/lang/"):
 		if "csv" not in i:
 			Language.add_item(i.split(".")[1])
 			if i.split(".")[1] == Global.config.lang: Language.selected = Language.item_count - 1
-	$ColorPicker.color = Color("#"+Global.config.background_color)
-	$DeckType.selected = Global.config.card_pack
 
 # Изменение языка
 func _on_language_item_selected(index: int) -> void:
 	TranslationServer.set_locale(Language.get_item_text(index))
-	Global.config.lang = Language.get_item_text(index)
-	Global.save_config()
+	_save_config("lang", Language.get_item_text(index))
 
 # Изменение карточного пака
-func _on_deck_type_item_selected(index: int) -> void:
-	Global.config.card_pack = index
-	Global.save_config()
+func _on_deck_type_item_selected(index: int) -> void: _save_config("card_pack", index)
 
 # Изменение цвета фона
 func _on_color_picker_color_changed(color: Color) -> void:
 	Global.main.get_child(0).modulate = color
-	Global.config.background_color = color.to_html()
+	_save_config("background_color", color.to_html())
+
+# Сохранение изменений
+func _save_config(param_name: String, value: Variant) -> void:
+	Global.config[param_name] = value
 	Global.save_config()
 
 # Сброс цвета
