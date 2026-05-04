f76fe987bd36e9024f2bb16ade80fbf74e5d5861
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-05-04 16:00:48 +0800

    change the inscription on the move button

--- playing-the-fool/scripts/pages/game.gd
+++ playing-the-fool/scripts/pages/game.gd
@@ -1,4 +1,6 @@
 extends GameWindow
+# Путь к объекту в сцене
+@onready var Dropping = $Dropping
 
 # Старт раздачи карт
 func _ready() -> void: set_stage(Global.GameStates.DISTRIBUTION)
@@ -6,4 +8,6 @@ func _ready() -> void: set_stage(Global.GameStates.DISTRIBUTION)
 # Изменение текущего состояния игры
 func set_stage(state: Global.GameStates) -> void:
 	Global.game_state = state
-	$Dropping.disabled = state == Global.GameStates.DISTRIBUTION
+	Dropping.disabled = state == Global.GameStates.DISTRIBUTION
+	if state == Global.GameStates.PLAY: Dropping.set_text(tr(["_ATTACK", "_PROTECT"][int(Global.player)]))
+	
