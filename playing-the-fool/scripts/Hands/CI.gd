3168d7ca5e6c7aed9398626c8bd235a6e84c3ed6
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-05-25 22:01:20 +0800

    Update CI.gd

--- playing-the-fool/scripts/Hands/CI.gd
+++ playing-the-fool/scripts/Hands/CI.gd
@@ -5,5 +5,5 @@ func _process(_delta: float) -> void: $"../CICardsCount".set_text(str(get_child_
 
 func shot() -> void:
 	get_child(0).reparent($"../Table")
-	$"../Table".get_child(0).new_pos = Vector2(40 + randi() % 994, 40 + randi() % 193)
+	$"../Table".get_child(0).new_pos = Vector2(100 + randi() % 874, 130)
 	$"../Table".get_child(0).show_hide()
