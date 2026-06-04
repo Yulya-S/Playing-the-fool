08b45fe6b0b64cd7ceccfd7fdccec4d41a5e5beb
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-06-04 13:45:41 +0800

    A message from the computer

--- playing-the-fool/scripts/Hands/CI.gd
+++ playing-the-fool/scripts/Hands/CI.gd
@@ -1,7 +1,10 @@
 extends Hand
 
 # Отображение количества карт в руке у компьютера
-func _process(_delta: float) -> void: $"../CICardsCount".set_text(str(get_child_count()))
+func _process(_delta: float) -> void:
+	$"../CICardsCount".set_text(str(get_child_count()))
+	if not Global.player and $"../Table".get_child_count() > 0 and not $"../CISay/".visible:
+		$"../CISay/AnimationPlayer".play("show_CISay")
 
 func shot() -> void:
 	if get_child(0) == null: return
