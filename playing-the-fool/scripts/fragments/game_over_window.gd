73f81872cc57944bdf6bb63dab5da89c8070995e
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-06-22 09:15:23 +0800

    saving statistics

--- playing-the-fool/scripts/fragments/game_over_window.gd
+++ playing-the-fool/scripts/fragments/game_over_window.gd
@@ -1,11 +1,17 @@
 extends ColorRect
+# Переменная
+var winner: int = 0 # Победитель в игре
 
 # Отображения окна победы
 func start_anim(player: bool, CI: bool) -> void:
 	$Result/AnimationPlayer.play("show")
-	var winner: int = 0 if player else 1
-	if winner == 0 and CI: $Result.set_text(tr("__USER2"))
+	winner = 2 if player and CI else not int(player)
+	if winner == 2: $Result.set_text(tr("__USER2"))
 	else: $Result.set_text(tr("_WINNER") + ": " + tr("__USER"+str(winner)))
+
+# Сохранение статистики по завершению анимации
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	Global.save_stats($"..".steps, winner, $"../Hand".get_child_count())
 
 # Обработка нажатия кнопки новой игры
 func _on_new_game_button_down() -> void:
