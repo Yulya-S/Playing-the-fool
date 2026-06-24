29e4c26108c4f254f480c9a23669d15c3d58532c
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-06-24 14:36:16 +0800

    Update deck.gd

--- playing-the-fool/scripts/fragments/deck.gd
+++ playing-the-fool/scripts/fragments/deck.gd
@@ -9,31 +9,34 @@ var user_idx: bool = true # Порядок передачи карты
 
 # Создание сцены
 func _ready() -> void:
-	Global.deck = self
+	Global.deck = self # Сохранение сцены в глобальном скрипте
 	# Создание и перемешивание калоды
 	var card_array: Array = Array(range(0, 36))
 	while len(card_array) > 0:
 		Global.add_obj(Cards, Global.card_scene)
 		Cards.get_child(-1).set_value(card_array.pop_at(randi() % len(card_array)))
-		Cards.get_child(-1).position.x += (36 - len(card_array)) * 0.5
-		Cards.get_child(-1).position.y -= (36 - len(card_array)) * 0.5
+		var value = (36 - len(card_array)) * 0.5
+		Cards.get_child(-1).position += Vector2(value, -value)
 	# Обновление графических данных
 	$Suit.frame = Cards.get_child(0).suit
 
 # Включение и отключение таймера раздачи карт
 func _process(_delta: float) -> void:
-	if Global.game_state == Global.GameStates.DISTRIBUTION and ((Player.cards_enough() and CI.cards_enough()) or Cards.get_child_count() == 0):
+	if Global.game_state == Global.GameStates.DISTRIBUTION and (_cards_enough() or Cards.get_child_count() == 0):
 			get_parent().set_stage(Global.GameStates.PLAY)
+
+# Проверка что количество карт у всех игроков >= 6
+func _cards_enough() -> bool: return Player.cards_enough() and CI.cards_enough()
+
+# Получение количества оставшихся в колоде карт
+func card_count() -> int: return Cards.get_child_count()
 
 # Раздача карт
 func _on_timer_timeout() -> void:
 	if Cards.get_child_count() <= 24: user_idx = Global.player
-	if CI.cards_enough() and Player.cards_enough(): return
+	if _cards_enough(): return
 	elif CI.cards_enough(): user_idx = true
 	elif Player.cards_enough(): user_idx = false
 	(Player if user_idx else CI).add_card()
 	user_idx = not user_idx
 	$CardsCount.set_text(str(Cards.get_child_count()))
-
-# Получение количества оставшихся в колоде карт
-func card_count() -> int: return Cards.get_child_count()
