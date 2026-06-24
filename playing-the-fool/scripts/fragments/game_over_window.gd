7210e168cf1b844d83e85bf69225b825d3d3cd68
Yulya-S <yulya_Shabsnova_03@mail.ru>
2026-06-24 15:04:31 +0800

    code cleaning

--- playing-the-fool/scripts/fragments/game_over_window.gd
+++ playing-the-fool/scripts/fragments/game_over_window.gd
@@ -1,17 +1,23 @@
 extends ColorRect
-# Переменная
+# Пути к объектам в сцене
+@onready var PL = $"../Hand"
+@onready var CI = $"../Computer"
+@onready var Res = $Result
 
-# Отображения окна победы
-func start_anim(player: bool, CI: bool) -> void:
-	$Result/AnimationPlayer.play("show")
-	if $Result.text == "":
-		if player and CI: _set_winner(2, tr("__USER2"))
-		else: _set_winner(int(not player), tr("_WINNER") + ": " + tr("__USER"+str(int(not player))))
+# Проверка завершилась ли игра
+func _process(delta: float) -> void:
+	if visible: return
+	if Global.deck.card_count() == 0 and Global.table.get_child_count() == 0 and (PL.empty() or CI.empty()):
+		var values: Array = [PL.empty(), CI.empty()]
+		$Result/AnimationPlayer.play("show")
+		if Res.text == "":
+			if false in values: _set_winner(values.find(true), tr("_WINNER") + ": " )
+			else: _set_winner()
 
 # Применение текста завершения игры
-func _set_winner(winner: int, text: String) -> void:
-	Global.save_stats($"..".steps, winner, $"../Hand".get_child_count())
-	$Result.set_text(text)
+func _set_winner(winner: int = 2, text: String = "") -> void:
+	Global.save_stats($"..".steps, winner, PL.get_child_count())
+	Res.set_text(text + tr("__USER" + str(winner)))
 
 # Обработка нажатия кнопки новой игры
 func _on_new_game_button_down() -> void: $".."._on_window_button_down("game")
--- playing-the-fool/scripts/pages/game.gd
+++ playing-the-fool/scripts/pages/game.gd
@@ -1,20 +1,19 @@
 extends GameWindow
+# Пути к объектам в сцене
+@onready var Trash = $Trash
+@onready var PL = $Hand
+@onready var CI = $Computer
+
 # Переменные
 var steps: int = 0 # Счетчик ходов для статистики
 var first_clear: bool = false # Была ли объявленна первая "бита"
-var clear_table: bool = false # остановка действий во время чистки стола
 
 # Старт раздачи карт
 func _ready() -> void: set_stage(Global.GameStates.DISTRIBUTION)
 
-# Завершение игры
+# Очистка стола
 func _process(_delta: float) -> void:
-	if $Trash.get_child_count() == 0: clear_table = false
-	for i in $Trash.get_children():
-		if $Trash.position.x - i.position.x < 20: Global.delete_child($Trash, i)
-	if $GameOverWindow.visible: return
-	if Global.deck.card_count() == 0 and Global.table.get_child_count() == 0 and ($Hand.empty() or $Computer.empty()):
-		$GameOverWindow.start_anim($Hand.empty(), $Computer.empty())
+	for i in Trash.get_children(): if Trash.position.x - i.position.x < 20: Global.delete_child(Trash, i)
 
 # Изменение текущего состояния игры
 func set_stage(state: Global.GameStates) -> void:
@@ -22,24 +21,23 @@ func set_stage(state: Global.GameStates) -> void:
 	Global.deck.get_child(-1).paused = state == Global.GameStates.PLAY
 	if state == Global.GameStates.PLAY:
 		$Dropping.set_text(tr(["_ATTACK", "_PROTECT"][int(Global.player)]))
-		$Hand._card_sort()
-		if Global.player: $Computer.attack()
+		PL._card_sort()
+		if Global.player: CI.attack()
 	else: steps += 1
 	Global.table.card_prices = []
 
 # Завершение хода и начало следующего
 func next_step() -> void:
-	clear_table = true
 	first_clear = true
 	Global.player = not Global.player
 	for i in Global.table.get_children():
-		i.reparent($Trash)
-		i.new_pos.x = $Trash.position.x
+		i.reparent(Trash)
+		i.new_pos.x = Trash.position.x
 
 # Обработка нажатия на кнопку завершения хода игрока
 func _on_dropping_button_down() -> void:
 	if Global.table.get_child_count() == 0 or Global.game_state != Global.GameStates.PLAY: return
-	if Global.player: $Hand.taking_cards()
-	elif $CISay.visible: $Computer.taking_cards()
+	if Global.player: PL.taking_cards()
+	elif $CISay.visible: CI.taking_cards()
 	else: next_step()
 	set_stage(Global.GameStates.DISTRIBUTION)
